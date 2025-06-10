bdbc6bc3823f4132e8b1f2d053bb602c2f19d763
TALHAA23 <talhasifat23@gmail.com>
2025-06-10 13:23:36 +0500

    worker url corrected

--- app/components/chat-bot.tsx
+++ app/components/chat-bot.tsx
@@ -44,7 +44,7 @@ export default function ChatBot({ chatbot }: Props) {
         setChats((prev) => [...prev, myMessage]);
 
         const response = await fetch(
-          `https://6913309b-tone-talk-worker.talhaa23.workers.dev/${chatbot}`,
+          `https://6913309b-tone-talk-worker.talhaa23.workers.dev/api/${chatbot}`,
           // `http://127.0.0.1:8787/api/${chatbot}`,
           {
             method: "POST",
